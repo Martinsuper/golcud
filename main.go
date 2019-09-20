1963161e44307e59c7c28d2ffc4277332c3674cd
luyao.duan <mrtduan@gmail.com>
2019-09-20 16:31:50 +0800

    修改目录结构

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	// "gitee.com/martind/golcud"
-	"github.com/Martinsuper/golcud"
+	"gitee.com/martind/golcud"
+	// "github.com/Martinsuper/golcud"
 )
 func main() {
 	golcud.SendMessage("hello world! today")
